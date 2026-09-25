47d306337ca9a0240ace25a562499e3e0f009c2a
Nitisraj T R <nitisraj2008@gmail.com>
2026-09-25 21:15:33 +0530

    Day 6: Arrays - Find 2nd and 3rd largest elements

---
+++ day6_Q1.py
@@ -0,0 +1,24 @@
+a=list(map(int,(input('Enter elements of the list separated by space: ').split())))
+a.sort(reverse=True)
+z=a[0]
+b=[]
+if len(a)<3:
+    print('Not enough elements(need at least 3)')
+else:
+    for i in a:
+        if i!=z:
+            b.append(i)
+    b.sort(reverse=True)
+    if len(b)==0:
+        b=a[1:]
+    y=b[0]
+    c=[]
+    for i in b:
+        if i!=y:
+            c.append(i)
+    c.sort(reverse=True)
+    if len(c)==0:
+        c=b[1:]
+    x=c[0]
+    print('2nd Largest:',y)
+    print('3rd Largest:',x)
