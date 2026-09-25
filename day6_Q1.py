8d7e33e4ad2fa57f2571e54025609542ca3e2732
Nitisraj T R <nitisraj2008@gmail.com>
2026-09-25 21:49:30 +0530

    Day 6: Arrays - Find 2nd and 3rd largest elements

--- day6_Q1.py
+++ day6_Q1.py
@@ -1,26 +1,20 @@
 a=list(map(int,(input('Enter elements of the list separated by space: ').split())))
 a.sort(reverse=True)
+n=a[-1]
 z=a[0]
-b=[]
+b=[z]
 if len(a)<3:
     print('Not enough elements(need at least 3)')
+elif z==n:
+    print('All elements are same,not enough unique elements,so 2nd and 3rd largest are same')
 else:
     for i in a:
-        if i!=z:
+        if i!=b[-1]:
             b.append(i)
-    b.sort(reverse=True)
-    if len(b)==0:
-        b=a[1:]
-    y=b[0]
-    c=[]
-    for i in b:
-        if i!=y:
-            c.append(i)
-    c.sort(reverse=True)
-    if len(c)==0:
-        c=b[1:]
-    x=c[0]
-    if x==y:
-        print('Not enough unique elements,so 2nd and 3rd largest are same')
-    print('2nd Largest:',y)
-    print('3rd Largest:',x)
+    if len(b)==2:
+        print('Not enough unique elements,so 3rd largest is same as 2nd largest')
+        print('2nd Largest:',b[1])
+        print('3rd Largest:',b[1])
+    else:
+        print('2nd Largest:',b[1])
+        print('3rd Largest:',b[2])
